9d0e24069384476c2ab956dd8c9ca3c47f096ce9
Clement Balestrat <clement.balestrat@gmail.com>
2020-06-10 15:52:24 +1000

    readme updated

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # synthetix-exchange-stats
 
 API endpoints for CMC and Coingecko
+
+- `/api/total-supply`: SNX total supply
+- `api/pairs/:category`: Available synths pairs for a specified category in `['all', 'crypto', 'forex', 'commodity', 'index', 'equities']`
+- `api/ticker/:pair`: Returns `rate, bid, ask, volume24hFrom, volume24hTo, low24hRate, high24hRate` for a specified synth pair
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 
 const getTotalSupply = require('./total-supply');
 const getPairs = require('./pairs');
-const getTransactions = require('./transactions');
 const getTicker = require('./ticker');
 
 const router = express.Router();
@@ -13,7 +12,6 @@ router.get('/alive', (req, res) => {
 
 router.get('/api/total-supply', getTotalSupply);
 router.get('/api/pairs/:category', getPairs);
-router.get('/api/transactions/:pair', getTransactions);
 router.get('/api/ticker/:pair', getTicker);
 
 module.exports = router;
